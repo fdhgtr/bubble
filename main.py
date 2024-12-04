62eb099bc3c72c5b6ba63c60ddd8b193b5708c6d
Tobias <159123644+fdhgtr@users.noreply.github.com>
2024-12-04 09:18:07 +0000

    gh

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 arr = [34, 7, 23, 32, 5, 62, 32, 9, 54, 18]
 print("Původní pole:", arr)
 
@@ -13,3 +12,19 @@ def bubble_sort(arr):
 
 sorted_arr = bubble_sort(arr)
 print("Seřazené pole:", sorted_arr)
+
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+sorted_arr_selection = selection_sort(arr.copy())
+print("Seřazené pole (Selection Sort):", sorted_arr_selection)
