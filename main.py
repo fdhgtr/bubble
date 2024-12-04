6f257ceb2506a4f71f15f5f78ef08589fba1c9ab
Tobias <159123644+fdhgtr@users.noreply.github.com>
2024-12-04 09:29:03 +0000

    s

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 array = [34, 7, 23, 32, 5, 62, 32, 9, 54, 18]
 print("Původní pole:", array)
 
@@ -12,6 +13,16 @@ def bubble_sort(array):
 
 sorted_arr = bubble_sort(array)
 print("Seřazené pole:", sorted_arr)
+
+def bogosort(arrayb):
+    count = 0
+    while not is_sorted(arrayb):
+        random.shuffle(arrayb)
+        count += 1
+        print(f"Pokus {count}: {arrayb}")
+    print(f"Sorted array after {count} shuffles: ")
+    bogosort(arrayb)
+    print("Bogo",arrayb)
 
 
 def selection_sort(array):
