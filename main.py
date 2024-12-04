fd6d4cd62494b911a14672b74a57f94753a00717
Tobias <159123644+fdhgtr@users.noreply.github.com>
2024-12-04 09:05:08 +0000

    k

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ arr = [64, 34, 25, 12, 22, 11, 90]
 sorted_arr = bubble_sort(arr)
 print(sorted_arr)
 
-def is_sorted(arr):
+def is_sorted(arra):
     
-    return all(arr[i] <= arr[i+1] for i in range(len(arr)-1))
+    return all(arra[i] <= arra[i+1] for i in range(len(arr)-1))
 
-def bogosort(arr):
-    while not is_sorted(arr):
-        random.shuffle(arr)  
-    return arr
+def bogosort(arra):
+    while not is_sorted(arra):
+        random.shuffle(arra)  
+    return arra
 
 
-arr = [64, 34, 25, 12, 22, 11, 90]
-sorted_arr = bogosort(arr)
-print("Sorted array:", sorted_arr)
+arra = [65, 35, 20, 10, 28, 19, 9]
+sorted_arra = bogosort(arra)
+print(sorted_arra)
