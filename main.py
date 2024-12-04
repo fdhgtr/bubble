d90d4fcec928f014c76d6981ef7cb1d6f1244347
Tobias <159123644+fdhgtr@users.noreply.github.com>
2024-12-04 09:11:02 +0000

    f

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-import random
+
+arr = [34, 7, 23, 32, 5, 62, 32, 9, 54, 18]
+print("Původní pole:", arr)
+
+
 def bubble_sort(arr):
-    n = len(arr)
-    
-    
-    for i in range(n):
-        
-        for j in range(0, n-i-1):
-            
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]  # Prohodíme hodnoty
     return arr
 
 
-arr = [64, 34, 25, 12, 22, 11, 90]
 sorted_arr = bubble_sort(arr)
-print(sorted_arr)
-
-def is_sorted(arra):
-    
-    return all(arra[i] <= arra[i+1] for i in range(len(arr)-1))
-
-def bogosort(arra):
-    while not is_sorted(arra):
-        random.shuffle(arra)  
-    return arra
-
-
-arra = [65, 35, 20, 10, 28, 19, 9]
-sorted_arra = bogosort(arra)
-print(sorted_arra)
+print("Seřazené pole:", sorted_arr)
