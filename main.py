f0c62c8c424fd02420807b4b56e92114155996ff
Tobias <159123644+fdhgtr@users.noreply.github.com>
2024-12-04 09:22:39 +0000

    s

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-arr = [34, 7, 23, 32, 5, 62, 32, 9, 54, 18]
-print("Původní pole:", arr)
+array = [34, 7, 23, 32, 5, 62, 32, 9, 54, 18]
+print("Původní pole:", array)
 
 
-def bubble_sort(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr) - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j] 
-    return arr
+def bubble_sort(array):
+    for i in range(len(array)):
+        for j in range(len(array) - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j] 
+    return array
 
 
-sorted_arr = bubble_sort(arr)
+sorted_arr = bubble_sort(array)
 print("Seřazené pole:", sorted_arr)
 
 
 def selection_sort(arr):
     for i in range(len(arr)):
         
-        min_index = i
+        nejmenší = i
         for j in range(i + 1, len(arr)):
-            if arr[j] < arr[min_index]:
-                min_index = j
+            if arr[j] < arr[nejmenší]:
+                nejmenší1 = j
         
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+        arr[i], arr[nejmenší1] = arr[nejmenší1], arr[i]
     return arr
 
 
